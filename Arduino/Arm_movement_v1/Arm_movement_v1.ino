a2372ea68aabc4cf69272e8ae4993776186c6f97
dragenair <utkarshjain0708@gmail.com>
2026-03-11 15:32:56 +0000

    updationg git

---
+++ Arduino/Arm_movement_v1/Arm_movement_v1.ino
@@ -0,0 +1,76 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// define the structure i.e.the dictionary / template.
+struct ServoConfig {
+  const char* name; // The "Key" (e.g., "base", "claw")
+  const int pin;    // The hardware pin
+  const int minPW;  // Minimum Pulse Width
+  const int maxPW;  // Maximum Pulse Width
+};
+
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
+
+const ServoConfig myServos[] = {
+  {"body",  0,  500,  2400},  //{motor 1 min, motor 1 max , motor 2 min, motor 2 max}
+  {"armL",  1,  600,  2200},
+  {"armR",  2,  750,  2000},
+  {"leg",   3,  500,  2500}
+};
+
+void printServoDictionary() {
+  Serial.println("--- Servo Configuration ---");
+  Serial.println("Name\t\tPin\t\tMin\t\tMax"); // Header with tabs
+  
+  for (int i = 0; i < sizeof(myServos) / sizeof(myServos[0]); i++) {
+    Serial.print(myServos[i].name);
+    Serial.print("\t\t"); // Tab for spacing
+    Serial.print(myServos[i].pin);
+    Serial.print("\t\t");
+    Serial.print(myServos[i].minPW);
+    Serial.print("\t\t");
+    Serial.println(myServos[i].maxPW);
+  }
+  Serial.println("---------------------------");
+}
+
+void moveServo(ServoConfig myServos, int angle) {
+  // 1. Get the min and max pulses from your dictionary
+  int minP = myServos.minPW;
+  int maxP = myServos.maxPW;
+
+  // 2. Map the angle (0-180) to the pulse range (min-max)
+  // map(value, fromLow, fromHigh, toLow, toHigh)
+  int pulse = map(angle, 0, 180, minP, maxP);
+
+  // 3. Send the command to the board
+  //board1.setPWM(myServos.pin, 0, pulse);
+  board1.writeMicroseconds(myServos.pin ,pulse);
+
+  Serial.print("Moving ");
+  Serial.print(myServos.name);
+  Serial.print(" to ");
+  Serial.print(angle);
+  Serial.println(" degrees.");
+}
+
+void setup() {
+  Serial.begin(9600); // for future printing errors and stuff
+  printServoDictionary();
+
+  board1.begin();
+  board1.setOscillatorFrequency(27000000); // sets the internal clock frequency to 27 MHz
+  board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+}
+
+void loop() {
+
+  printServoDictionary();
+  moveServo(myServos[0], 90);
+  delay(1000);
+  Serial.println(board1.getPWM(myServos[0].pin));
+  moveServo(myServos[0], 0);
+  delay(1000);
+  Serial.println(board1.getPWM(myServos[0].pin));
+}
